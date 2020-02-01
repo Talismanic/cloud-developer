c193a17bef6e5df65f841184e55c261a566f0413
Talismanic <mehedi.md.hasan@grameenphone.com>
2020-02-01 17:33:55 +0600

    removing the import command

--- course-02/exercises/udacity-c2-restapi/src/server.ts
+++ course-02/exercises/udacity-c2-restapi/src/server.ts
@@ -1,11 +1,9 @@
-const express = require("express");
-// import express from 'express';
+import express from 'express';
 import { sequelize } from './sequelize';
 
 import { IndexRouter } from './controllers/v0/index.router';
 
-// import bodyParser from 'body-parser';
-const bodyParser = require("body-parser");
+import bodyParser from 'body-parser';
 
 import { V0MODELS } from './controllers/v0/model.index';
 
